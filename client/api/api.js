73101c0a46209bb059e331e6fce42f3a0cdf1913
TuanAnh <ncq998@gmail.com>
2019-06-02 17:21:51 +0700

    dsadasdsasa

--- client/api/api.js
+++ client/api/api.js
@@ -6,7 +6,7 @@ import {TrackLocation} from "../react/common/location-tracker";
 
 
 const authenApiConfig = {
-  hostURL: "http://localhost:6969/api",
+  hostURL: `${process.env.APP_URI}/api`,
   onErrors: {
     "token_expired": () => {
       authenCache.clearAuthen();
@@ -28,7 +28,7 @@ const authenApiConfig = {
 };
 
 const offlineApiConfig = {
-  hostURL: "http://localhost:6969/api"
+  hostURL: `${process.env.APP_URI}/api`
 };
 
 
