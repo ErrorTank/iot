2c99b2a82f35e76b7b58766b8545e4699ae7fea3
TuanAnh <ncq998@gmail.com>
2019-06-02 19:24:20 +0700

    dasdas

---
+++ webpack.prod.config.js
@@ -0,0 +1,46 @@
+
+const path = require("path");
+const dotenv =require("dotenv");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    loader: ["@babel/polyfill", "./client/react/loader.jsx"]
+  },
+  output: {
+    filename: 'bundle.js',
+    publicPath: "/",
+    path:  path.resolve(__dirname, 'public/bundle'),
+
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".styl", ".graphql"]
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env",  "@babel/preset-react"]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }, {
+        test: /\.styl$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "stylus-loader"
+        ]
+      }
+    ]
+  },
+};
