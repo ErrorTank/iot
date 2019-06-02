8e3aceaedc13d8f64f583ba11fc6cfbdd0ced73c
TuanAnh <ncq998@gmail.com>
2019-06-02 21:19:48 +0700

    3123123123

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,44 +5,50 @@ const webpack = require("webpack");
 
 
 
-module.exports = {
-  mode: "production",
-  entry: {
-    loader: ["@babel/polyfill", "./client/react/loader.jsx"]
-  },
-  output: {
-    filename: 'bundle.js',
-    publicPath: "/",
-    path:  path.resolve(__dirname, 'dist/bundle'),
+module.exports = (env) => {
+  console.log(env)
+  return ({
 
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".styl", ".graphql"]
-  },
-  node: {
-    fs: 'empty'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ["@babel/preset-env",  "@babel/preset-react"]
+    mode: "production",
+    entry: {
+      loader: ["@babel/polyfill", "./client/react/loader.jsx"]
+    },
+    output: {
+      filename: 'bundle.js',
+      publicPath: "/",
+      path:  path.resolve(__dirname, 'dist/bundle'),
+
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".styl", ".graphql"]
+    },
+
+    node: {
+      fs: 'empty'
+    },
+    module: {
+      rules: [
+
+        {
+          test: /\.jsx?$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ["@babel/preset-env",  "@babel/preset-react"]
+              }
             }
-          }
-        ],
-        exclude: /node_modules/
-      }, {
-        test: /\.styl$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "stylus-loader"
-        ]
-      }
-    ]
-  },
-};
+          ],
+          exclude: /node_modules/
+        }, {
+          test: /\.styl$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "stylus-loader"
+          ]
+        }
+      ]
+    },
+  });
+}
