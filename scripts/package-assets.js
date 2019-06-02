fe3a105b018beb8643d4986f30be4b3cb84f880c
TuanAnh <ncq998@gmail.com>
2019-06-02 20:24:55 +0700

    312312312

--- scripts/package-assets.js
+++ scripts/package-assets.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-const listDirFiles = ["assets", "files"];
+const listDirFiles = ["assets", "bundle"];
 const source = "../public";
 const dest = "../dist";
 
